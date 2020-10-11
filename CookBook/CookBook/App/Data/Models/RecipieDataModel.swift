d2e46e3fc0f7cc83dd446455f822e22d124a8480
obashir <osama.bashir@zalando.de>
2020-10-11 19:00:34 +0200

    refactor recipe model

--- CookBook/CookBook/App/Data/Models/RecipieDataModel.swift
+++ CookBook/CookBook/App/Data/Models/RecipieDataModel.swift
@@ -10,20 +10,22 @@ import Contentful
 
 struct RecipieDataModel {
     
+    static let contentId = "recipe"
+    
     let title: String?
     let description: String?
     let tags: [String]
-    let chefName: String?
+    let chef: String?
     let imageURL: URL?
     
     init(title: String?,
          description: String?,
-         chefName: String?,
+         chef: String?,
          imageURL: URL?,
          tags: [String]) {
         self.title = title
         self.description = description
-        self.chefName = chefName
+        self.chef = chef
         self.imageURL = imageURL
         self.tags = tags
     }
@@ -31,15 +33,16 @@ struct RecipieDataModel {
     init(with entry: Entry) {
         let title = entry.fields["title"] as? String
         let description = entry.fields["description"] as? String
-        let chefName = entry.fields.linkedEntry(at: "chef")?.fields["name"] as? String
+        let chef = entry.fields.linkedEntry(at: "chef")?.fields["name"] as? String
         let imageURL = entry.fields.linkedAsset(at: "photo")?.url
         let tags: [String]
+        
         if let tagEntries = entry.fields.linkedEntries(at: "tags") {
             tags = tagEntries.compactMap { $0.fields["name"] as? String }
         } else {
             tags = []
         }
-        self.init(title: title, description: description, chefName: chefName, imageURL: imageURL, tags: tags)
+        self.init(title: title, description: description, chef: chef, imageURL: imageURL, tags: tags)
     }
     
 }
